02f202e22794bca84cd8bfc04412b33036dd5601
Isaac Landin <isaac.landin@gmail.com>
2025-11-05 12:45:00 -0600

    updated longest consecutive for O(n) solution

--- Python/longestConsecutive.py
+++ Python/longestConsecutive.py
@@ -1,26 +1,12 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        num_map = {}
+        numSet = set(nums)
         longest = 0
-        nums.sort()
-        print(nums)
 
-        for num in nums:
-            print("current num: " + str(num))
-            if not nums:
-                return 0
-            if len(nums) == 1:
-                return 1
-            print(num_map.get(num-1, 0))
-            if num_map.get(num-1, 0) == 0:
-                num_map.update({num: 1})
-                if longest == 0:
-                    longest += 1
-            else:
-                temp = (num_map.get(num-1, 0) + 1) 
-                num_map.update({num: temp})
-                if temp > longest:
-                    longest = temp
-            print("current longest: " + str(longest))
-
+        for num in numSet:
+            if (num - 1) not in numSet:
+                length = 1
+                while (num + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
         return longest
